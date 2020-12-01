7113bdb2e2e4dd555fbc702b5160689f41c72fdb
Bhav <bhavikmakwana09@gmail.com>
2020-12-01 15:46:44 +0000

    Day 1

---
+++ day1/target/debug/day1.d
@@ -0,0 +1,1 @@
+/Users/bhavik/OneDrive/Projects/AdventOfCode/2020/day1/day1/target/debug/day1: /Users/bhavik/OneDrive/Projects/AdventOfCode/2020/day1/day1/src/main.rs
---
+++ day1/target/debug/deps/day1.d
@@ -0,0 +1,5 @@
+/Users/bhavik/OneDrive/Projects/AdventOfCode/2020/day1/day1/target/debug/deps/day1: src/main.rs
+
+/Users/bhavik/OneDrive/Projects/AdventOfCode/2020/day1/day1/target/debug/deps/day1.d: src/main.rs
+
+src/main.rs:
