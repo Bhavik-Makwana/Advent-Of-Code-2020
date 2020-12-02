896188b77c57364f1016c495d7903489417afee1
Bhav <bhavikmakwana09@gmail.com>
2020-12-02 23:52:39 +0000

    Rewrote day1 solutions

--- calendar/src/day1.rs
+++ calendar/src/day1.rs
@@ -1,26 +1,22 @@
 use std::collections::HashSet;
 use std::iter::FromIterator;
+use std::fmt::Error;
 
-pub fn part_one(years: &Vec<i32>) -> i32 {
+pub fn part_one(years: &Vec<i32>) -> Result<i32, Error> {
     let s: HashSet<i32> = HashSet::from_iter(years.iter().cloned());
-    for year in years.iter() {
-        let x = 2020 - year;
-        if s.contains(&x) {
-            return year * (2020 - year);
-        }
-    }
-    -1
+    let vec: Vec<&i32> = years.into_iter().filter(|year| s.contains(&(2020-*year))).collect::<Vec<_>>();
+    Ok(vec[0] * vec[1])
 }
 
-pub fn part_two(years: &Vec<i32>) -> i32 {
+pub fn part_two(years: &Vec<i32>) -> Result<i32, Error> {
     let s: HashSet<i32> = HashSet::from_iter(years.iter().cloned());
     for i in 0..s.len() {
         for j in i..s.len() {
             let x = 2020 - years[i] - years[j];
             if s.contains(&x) {
-                return x * years[i] * years[j];
+                return Ok(x * years[i] * years[j])
             }
         }
     }
-    -1
+    Err(Error)
 }
--- calendar/src/main.rs
+++ calendar/src/main.rs
@@ -7,8 +7,8 @@ pub use crate::readfile::fileio;
 fn main() {
     let vec = fileio::read_file_int(String::from("input/day1.txt"));
 
-    let ans = day1::part_one(&vec);
+    let ans = day1::part_one(&vec).unwrap();
     println!("part one: {:#?}", ans);
-    let ans2 = day1::part_two(&vec);
+    let ans2 = day1::part_two(&vec).unwrap();
     println!("part two: {:#?}", ans2);
 }
