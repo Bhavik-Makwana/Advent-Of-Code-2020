1ddb4e199a81072933124cf5bdb71546570d9540
Bhav <bhavikmakwana09@gmail.com>
2020-12-02 11:25:48 +0000

    Working sollution but still not good rust code

---
+++ day2/target/debug/day2.d
@@ -0,0 +1,1 @@
+/Users/bhavik/OneDrive/Projects/AdventOfCode/2020/day2/target/debug/day2: /Users/bhavik/OneDrive/Projects/AdventOfCode/2020/day2/src/main.rs
---
+++ day2/target/debug/deps/day2.d
@@ -0,0 +1,5 @@
+/Users/bhavik/OneDrive/Projects/AdventOfCode/2020/day2/target/debug/deps/day2: src/main.rs
+
+/Users/bhavik/OneDrive/Projects/AdventOfCode/2020/day2/target/debug/deps/day2.d: src/main.rs
+
+src/main.rs:
