826a33840d25f30eb1070ab565c0cb1d7f3c465a
Bhav <bhavikmakwana09@gmail.com>
2020-12-06 01:27:36 +0000

    redone day 5

--- calendar/src/day5.rs
+++ calendar/src/day5.rs
@@ -1,78 +1,42 @@
-use regex::Regex;
-use std::cmp;
 use std::fmt::Error;
 
-pub fn part_one(tickets: &Vec<String>) -> Result<i32, Error> {
-    let mut _total = 0;
-    let mut max = 0;
-    for ticket in tickets.iter() {
-        let row = get_row(&ticket[0..7]).unwrap();
-        let col = get_col(&ticket[7..10]).unwrap();
-        max = cmp::max(max, row * 8 + col);
-    }
-
-    Ok(max)
+pub fn part_one(tickets: &Vec<String>) -> Option<i32> {
+    tickets.iter().map(|ticket| get_seat_id(ticket)).max()
 }
 
 pub fn part_two(tickets: &Vec<String>) -> Result<i32, Error> {
-    let mut seat_ids = Vec::new();
-    for ticket in tickets.iter() {
-        let row = get_row(&ticket[0..7]).unwrap();
-        let col = get_col(&ticket[7..10]).unwrap();
-        seat_ids.push(row * 8 + col);
-    }
-
-    seat_ids.sort();
     let mut ans: i32 = 0;
-    for i in (1..seat_ids.len() - 1) {
+    let mut seat_ids: Vec<i32> = tickets.iter().map(|ticket| get_seat_id(ticket)).collect();
+    seat_ids.sort();
+    // let x: Vec<_> = (1..seat_ids.len()-1).filter(|i| seat_ids[*i as usize] != seat_ids[(*i as usize)-1] +1).collect();
+    println!("{:?}", x);
+    for i in 1..seat_ids.len()-1 {
         if seat_ids[i] != seat_ids[i - 1] + 1 {
-            // || seat_ids[i] != seat_ids[i+1]-1
-            ans = seat_ids[i];;
+            ans = seat_ids[i];
         }
     }
     Ok(ans - 1)
 }
 
-fn get_row(ticket: &str) -> Result<i32, Error> {
-    let mut lb = 0;
-    let mut ub = 127;
-    let mut mp: f32 = 0.0;
-    for i in ticket.chars() {
-        mp = (lb as f32 + ub as f32) / 2.0;
-        match i {
-            'F' => {
-                mp = mp.floor();
-                ub = mp as i32;
-            }
-            'B' => {
-                mp = mp.ceil();
-                lb = mp as i32;
-            }
-            _ => {
-                return Err(Error);
-            }
-        }
-    }
-    Ok(mp as i32)
+fn _get_seat_id_functional(ticket: &str) -> i32 {
+    let row: String = String::from(&ticket[0..7]).chars()
+    .map(|x| if x == 'B' { '1'} else{ x})
+    .map(|x| if x == 'F' { '0'} else{ x})
+    .collect();
+    let col: String = String::from(&ticket[7..10]).chars()
+    .map(|x| if x == 'R' { '1'} else{ x})
+    .map(|x| if x == 'L' { '0'} else{ x})
+    .collect();
+    
+    let row_int = i32::from_str_radix(&row[..], 2).unwrap();
+    let col_int = i32::from_str_radix(&col[..], 2).unwrap();
+    row_int * 8 + col_int
 }
 
-fn get_col(ticket: &str) -> Result<i32, Error> {
-    let mut lb = 0;
-    let mut ub = 7;
-    let mut mp: f32 = 0.0;
-    for i in ticket.chars() {
-        mp = (lb as f32 + ub as f32) / 2.0;
-        match i {
-            'L' => {
-                mp = mp.floor();
-                ub = mp as i32;
-            }
-            'R' => {
-                mp = mp.ceil();
-                lb = mp as i32;
-            }
-            _ => return Err(Error),
-        }
-    }
-    Ok(mp as i32)
+fn get_seat_id(ticket: &str) -> i32 {
+    let row = &ticket[0..7].replace("B", "1").replace("F", "0");
+    let col = &ticket[7..10].replace("L", "0").replace("R", "1");
+    let row_int = i32::from_str_radix(&row, 2).unwrap();
+    let col_int = i32::from_str_radix(&col, 2).unwrap();
+    row_int * 8 + col_int
 }
