1ea73d7a0fa8b162d2af5280f880a54a4da9a4db
Bhav <bhavikmakwana09@gmail.com>
2020-12-18 10:29:44 +0000

    day 18

--- calendar/src/main.rs
+++ calendar/src/main.rs
@@ -1,18 +1,18 @@
 extern crate time;
 use time::PreciseTime;
 
-mod day17;
+mod day18;
 mod readfile;
 
 pub use crate::readfile::fileio;
 
 fn main() {
-    let vec = fileio::read_file_2d(String::from("input/day17.txt"));
+    let vec = fileio::read_file(String::from("input/day18.txt"));
 
-    let ans = day17::part_one(&vec);
+    let ans = day18::part_one(&vec);
     println!("part one: {:#?}", ans.unwrap());
     let start = PreciseTime::now();
-    let ans2 = day17::part_two(&vec).unwrap();
+    let ans2 = day18::part_two(&vec).unwrap();
     let end = PreciseTime::now();
     println!("part two: {:#?}", ans2);
     println!("took: {} seconds", start.to(end));
